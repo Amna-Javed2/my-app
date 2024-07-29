13d067ca20e314f1c536fbd750821dc24a67236a
Amna-Javed2 <amna32611@gmail.com>
2024-07-29 04:39:23 -0700

    link tag

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,24 @@
 const dashboard = () => {
-    return <div>dashboard</div>;
-  };
-  
-  export default dashboard;
+  return (
+    <div className="p-12">
+      dashboard
+      <div>
+        <p className="font-bold text-lg">Nested Routes</p>
+        <p>
+          To create a nested route, you can nest folders
+          inside...................new folders in the app directory.
+        </p>
+      </div>
+      <div className="flex flex-col gap-5">
+      {`html <a> tag`}
+      <p>
+        {`The <a> tag defines a hyperlink, which is used to link from one page to another.`}
+        </p>
+        <p>The most important attribute of the <a> element is the href attribute, which indicates the link's destination.</p>
+        <a>YouTube Link</a>
+        </div>
+    </div>
+  );
+};
+
+export default dashboard;
